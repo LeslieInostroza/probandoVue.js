42590c2e04e6f8a147f838110c847695ac2a1f6f
LeslieInostroza <leslie.ino@gmail.com>
2018-11-07 12:58:57 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+  el: '#app', // con # se llama al id  del div id=app, el detectac el id
+  data:{
+    //se pueden inventar seudovariables
+    titulo: 'GYM con vue', // llamamos al titulo 
+    tareas: [],
+    nuevaTarea:''
+  }
+});
